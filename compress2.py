2ac0a5b9795740f3586bf16a31809b0dc040c256
stone <1589647774@qq.com>
2024-06-29 10:32:03 +0800

    struct rearrange

---
+++ compress2.py
@@ -0,0 +1,14 @@
+import subprocess
+
+dotname = ".mftheme"
+
+source = input("input the folder >>>")
+target = source + dotname
+
+
+subprocess.run([
+    "pwsh",
+    "-Command",
+    f"Compress-Archive -Path {source} -destinationPath {target}"
+])
+
---
+++ note.md
@@ -0,0 +1,6 @@
+尝试直接从原图搜索特定几种颜色，用作ui的颜色。
+
+使用adobe color cc
+
+https://color.adobe.com/zh/create/image, 可以从上传的图片中寻找一些色彩。
+
